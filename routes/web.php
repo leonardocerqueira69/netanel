a5fd003d1b920b93f13cb047a3ab7702e70f3c78
Ian <iansantosfilgueiras@hotmail.com>
2024-06-29 13:19:14 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\DiretorController;
+use App\Http\Controllers\SetorController;
+use App\Http\Controllers\TarefaController;
+use App\Http\Controllers\PcpController;
+use App\Http\Controllers\ListaController;
+use App\Http\Controllers\ChecklistController;
+use App\Http\Controllers\CqController;
+use App\Http\Controllers\TipoChecklistController;
+use App\Http\Controllers\ColaboradorController;
+
+
+Route::resource('diretores', DiretorController::class);
+Route::resource('setores', SetorController::class);
+Route::resource('tarefas', TarefaController::class);
+Route::resource('pcps', PcpController::class);
+Route::resource('listas', ListaController::class);
+Route::resource('tarefas', ChecklistController::class);
+Route::resource('cqs', CqController::class);
+Route::resource('tipos-checklist', TipoChecklistController::class);
+Route::resource('colaboradores', ColaboradorController::class);
+
+
+
+
