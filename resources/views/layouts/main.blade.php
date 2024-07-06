4b1939750dea8a8b3089940c17c1661a3ba3c3b1
max <mxmascarenhasz7@gmail.com>
2024-07-05 22:31:49 -0300

    Efeito hover no menu dropdown

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,53 +1,63 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="favicon.png" type="image/png">
     <link rel="shortcut icon" href="favicon.png" type="image/png">
     <title>Netanel @yield('title')</title>
-    <!--BOOTSTRAP-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!--GOOGLE FONTS-->
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- GOOGLE FONTS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
-    <!--CSS APLICAÇÃO-->
+    <!-- CSS APLICAÇÃO -->
     <link rel="stylesheet" href="css/styles.css">
+    <style>
+        .dropdown:hover .dropdown-menu {
+            display: block;
+        }
+
+        .dropdown-menu {
+            display: none; /* Ocultar inicialmente */
+            opacity: 0; /* Mantém invisível até o fadeIn */
+            transition: opacity 0.3s; /* Animação de transição */
+        }
+
+        .dropdown-menu.show {
+            display: block; /* Exibir quando a classe .show é aplicada */
+            opacity: 1; /* Fazer aparecer gradualmente */
+        }
+    </style>
 </head>
-
-
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="/">
             <img src="/img/logo.png" alt="Logo" width="70" height="70">
         </a>
-        <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <img id="toggle-menu" src="/img/menu_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24 (1).png" width="30" height="30" alt="Menu">
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="pcpDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" id="pcpDropdown" role="button" aria-haspopup="true" aria-expanded="false">
                         PCP
                     </a>
-
-                    <div class="dropdown-menu" id="pcpMenu" aria-labelledby="pcpDropdown">
-
-                        <a class="dropdown-item" href=" {{ route('pcp.showPcp', ['id' => '1']) }}">EMBORRACHAMENTO</a>
-                        <a class="dropdown-item" href=" {{ route('pcp.showPcp', ['id' => '2']) }}">MONTAGEM</a>
-                        <a class="dropdown-item" href=" {{ route('pcp.showPcp', ['id' => '3']) }}">PROJETOS</a>
-                        <a class="dropdown-item" href=" {{ route('pcp.showPcp', ['id' => '4']) }}">EXPEDIÇÃO</a>
-                        <a class="dropdown-item" href=" {{ route('pcp.showPcp', ['id' => '5']) }}">OUTROS</a>
-
+                    <div class="dropdown-menu fade" id="pcpMenu" aria-labelledby="pcpDropdown">
+                        <a class="dropdown-item" href="{{ route('pcp.showPcp', ['id' => '1']) }}">EMBORRACHAMENTO</a>
+                        <a class="dropdown-item" href="{{ route('pcp.showPcp', ['id' => '2']) }}">MONTAGEM</a>
+                        <a class="dropdown-item" href="{{ route('pcp.showPcp', ['id' => '3']) }}">PROJETOS</a>
+                        <a class="dropdown-item" href="{{ route('pcp.showPcp', ['id' => '4']) }}">EXPEDIÇÃO</a>
+                        <a class="dropdown-item" href="{{ route('pcp.showPcp', ['id' => '5']) }}">OUTROS</a>
                     </div>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="checklistDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" id="checklistDropdown" role="button" aria-haspopup="true" aria-expanded="false">
                         Checklist
                     </a>
-                    <div class="dropdown-menu" id="checklistMenu" aria-labelledby="checklistDropdown">
+                    <div class="dropdown-menu fade" id="checklistMenu" aria-labelledby="checklistDropdown">
                         <a class="dropdown-item" href="{{ route('checklists.show', ['nome_tipo' => 'klabin']) }}">Checklist KLABIN</a>
                         <a class="dropdown-item" href="{{ route('checklists.show', ['nome_tipo' => 'penha']) }}">Checklist PENHA</a>
                         <a class="dropdown-item" href="{{ route('checklists.show', ['nome_tipo' => 'geral']) }}">Checklist GERAL</a>
@@ -63,37 +73,34 @@
 
     <div class="fixed-footer"></div>
 
+    <!-- SCRIPTS -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
     <script>
-        document.querySelectorAll('.dropdown-item').forEach(item => {
-            item.addEventListener('click', () => {
-                const tipo = item.getAttribute('data-tipo');
-                window.location.href = `/checklist/${tipo}`;
-            });
+        // Mostrar dropdown ao passar o mouse sobre o link
+        $('.dropdown-toggle').on('mouseover', function() {
+            var dropdownMenu = $(this).next('.dropdown-menu');
+            dropdownMenu.addClass('show');
         });
-    </script>
-    <script>
-        document.querySelectorAll('#checklistMenu li').forEach(item => {
-            item.addEventListener('click', () => {
-                const tipo = item.getAttribute('data-tipo');
-                window.location.href = `/checklist/${tipo}`;
-            });
-        });
-    </script>
 
-    <script>
-        document.querySelectorAll('#pcpMenu li').forEach(item => {
-            item.addEventListener('click', () => {
-                const id = item.getAttribute('data-tipo');
-                window.location.href = `/pcp/showPcp/${id}`;
-            });
+        // Manter dropdown visível ao passar o mouse sobre ele
+        $('.dropdown-menu').on('mouseover', function() {
+            $(this).addClass('show');
+        });
+
+        // Esconder dropdown ao tirar o mouse de cima do link principal e do dropdown
+        $('.nav-item.dropdown').on('mouseleave', function() {
+            var dropdownMenu = $(this).find('.dropdown-menu');
+            dropdownMenu.removeClass('show');
+        });
+
+        // Esconder dropdown ao tirar o mouse completamente do dropdown
+        $('.dropdown-menu').on('mouseleave', function(e) {
+            if (!$(e.relatedTarget).closest('.nav-item.dropdown').length) {
+                $(this).removeClass('show');
+            }
         });
     </script>
-
-    @yield('content')
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
-
 </html>
