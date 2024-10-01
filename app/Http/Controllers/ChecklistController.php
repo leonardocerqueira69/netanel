11f937dc250d50ef910e26e1867095b83397df20
leonardocerqueira69 <150107674+leonardocerqueira69@users.noreply.github.com>
2024-10-01 19:49:25 -0300

    Update ChecklistController.php

--- app/Http/Controllers/ChecklistController.php
+++ app/Http/Controllers/ChecklistController.php
@@ -14,16 +14,18 @@ class ChecklistController extends Controller
         // Código para listar todos os checklists
     }
 
-    public function show($nome_tipo)
-    {
-        $tipoChecklist = TipoCheckListModel::where('nome_tipo', $nome_tipo)->firstOrFail();
-        $checklists = CheckListModel::where('tipo', $tipoChecklist->id_tipo)->get();
+public function show($nome_tipo)
+{
+    $tipoChecklist = TipoCheckListModel::where('nome_tipo', $nome_tipo)->firstOrFail();
+    $checklists = CheckListModel::where('tipo', $tipoChecklist->id_tipo)->get();
 
-        return view('checklists.show', compact('tipoChecklist', 'checklists'));
-    }
+    return view('checklists.show', compact('tipoChecklist', 'checklists'));
+}
+
 
     public function create()
     {
+
         $tiposChecklist = TipoCheckListModel::all(); // Carrega todos os tipos de checklist para o dropdown
         return view('check.create', compact('tiposChecklist'));
     }
@@ -72,14 +74,31 @@ class ChecklistController extends Controller
         return redirect()->route('checklists.show', ['nome_tipo' => $checklist->tipoChecklist->nome_tipo])
             ->with('success', 'Checklist atualizado com sucesso!');
     }
+  
+  	public function updateAjax(Request $request)
+    {
+        $checklist = CheckListModel::find($request->id_checklist);
+
+        if ($checklist) {
+            $checklist->finalizado = $request->finalizado;
+            $checklist->save();
+            return response()->json(['success' => true]);
+        }
+
+        return response()->json(['success' => false], 404);
+    }
+
 
     public function destroy($id)
     {
+
         $check = CheckListModel::find($id);
+
 
         if (!$check) {
             return redirect()->route('welcome')->with('error', 'Checklist não encontrado.');
         }
+
 
         $tipoChecklist = $check->tipoChecklist;
 
@@ -89,24 +108,26 @@ class ChecklistController extends Controller
 
         $nome_tipo = $tipoChecklist->nome_tipo;
 
+
         $check->delete();
+
 
         return redirect()->route('checklists.show', ['nome_tipo' => $nome_tipo])->with('success', 'Checklist deletado com sucesso.');
     }
+  
+public function uncheckAll(Request $request)
+{
+    $validatedData = $request->validate([
+        'tipo' => 'required|exists:tipo_checklist,id_tipo',
+    ]);
 
-    public function uncheckAll(Request $request)
-    {
-        // Valida se o tipo de checklist foi fornecido
-        $validatedData = $request->validate([
-            'tipo' => 'required|exists:tipo_checklist,id_tipo',
-        ]);
+    CheckListModel::where('tipo', $validatedData['tipo'])
+                  ->where('finalizado', 1)
+                  ->update(['finalizado' => 0]);
 
-        // Atualiza todos os checklists do tipo fornecido para desmarcados
-        CheckListModel::where('tipo', $validatedData['tipo'])
-                      ->where('finalizado', 1)
-                      ->update(['finalizado' => 0]);
+    return redirect()->route('checklists.show', ['nome_tipo' => TipoCheckListModel::find($validatedData['tipo'])->nome_tipo])
+        ->with('success', 'Todos os checklists foram desmarcados.');
+}
 
-        return redirect()->route('checklists.show', ['nome_tipo' => TipoCheckListModel::find($validatedData['tipo'])->nome_tipo])
-            ->with('success', 'Todos os checklists foram desmarcados.');
-    }
+  
 }
