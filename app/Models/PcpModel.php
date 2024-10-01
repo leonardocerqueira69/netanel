ae9a81c5c353af881f69a9efb5aff27610b4c60d
leonardocerqueira69 <150107674+leonardocerqueira69@users.noreply.github.com>
2024-10-01 19:52:46 -0300

    Update PcpModel.php

--- app/Models/PcpModel.php
+++ app/Models/PcpModel.php
@@ -13,7 +13,7 @@ class PcpModel extends Model
     protected $primaryKey = 'id_pcp';
 
     protected $fillable = [
-        'setor', 'texto', 'data_atual', 'finalizado', 'andamento', 'arquivos', 'entrega', 'conclusao','cliente'
+        'setor', 'texto', 'data_atual', 'finalizado', 'andamento', 'arquivos', 'entrega', 'conclusao','cliente', 'meta_conclusao','iniciado'
     ];
     
     public function setor()
