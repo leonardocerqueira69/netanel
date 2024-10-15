cdb8742fcc3b3b76e60b1840ad559fd26ff9f013
Ian Filgueiras <109982573+Ian-Filgueiras@users.noreply.github.com>
2024-10-15 19:15:09 -0300

    Update showPcp.blade.php

--- resources/views/pcp/showPcp.blade.php
+++ resources/views/pcp/showPcp.blade.php
@@ -6,7 +6,8 @@
 <h1 id="notPCP">Não há PCP disponível no setor.</h1>
 @else
 
-<div id="identificadorCHK" class="text-center">
+<body style="background-color: #475159;">
+<div id="identificadorCHK" style="margin-top: 125px; background-color: #ffffff" class="text-center">
     <h2>PCP: {{ $setor->nome }}</h2>
 </div>
 
@@ -15,22 +16,41 @@
         @foreach($pcps as $pcp)
         <li class="pcpItem">
             <div id="tarefa">{!! $pcp->texto !!}</div>
-            <div id="finish">Finalizado: {{ $pcp->finalizado ? 'Sim' : 'Não' }}</div>
-            <div id="load">Andamento: {{ $pcp->andamento ? 'Sim' : 'Não' }}</div>
+<div class="pcpItem filha {{ $pcp->andamento ? 'andamento' : ($pcp->finalizado ? 'finalizado' : '') }}">
+    <div id="finish" class="{{ $pcp->finalizado ? 'finalizado' : 'nao-finalizado' }}">
+        Finalizado: {{ $pcp->finalizado ? 'Sim' : 'Não' }}
+    </div>
+
+    <div id="load" class="{{ $pcp->andamento ? 'andamento' : 'nao-andamento' }}">
+        Andamento: {{ $pcp->andamento ? 'Sim' : 'Não' }}
+    </div>
+</div>
             @if(isset($pcp->data_atual))
             <div id="dateUpdate">Criado em: {{ $pcp->data_atual }}</div>
             @endif
+                    @if(isset($pcp->meta_conclusao))
+          <div id="metaConclusao">
+            Meta de Conclusão: {{ \Carbon\Carbon::parse($pcp->meta_conclusao)->format('H:i') }}
+          </div>
+          @endif
+                    	@if(isset($pcp->iniciado))
+    			<div style="color: black;" id="dateStarted">Iniciado em: {{ $pcp->iniciado }}</div>
+			@endif
+                      @if(isset($pcp->conclusao))
+            <div id="conclusao">Concluído em: {{ $pcp->conclusao }}</div>
+            @endif
             @if(isset($pcp->entrega))
-            <div id="entrega">Entrega: {{ $pcp->entrega }}</div>
-                @if(isset($pcp->cliente))
-                <p>TRANSPORTADORA : {{$pcp->cliente}}</p>
-                @endif
+            <div id="entrega">Entrega ao Cliente: {{ $pcp->entrega }}</div>
             @endif
-            @if(isset($pcp->conclusao))
-            <div id="conclusao">Conclusao: {{ $pcp->conclusao }}</div>
+          
+            @if(isset($pcp->cliente))
+               <div><p id="transp">Transportadora : {{$pcp->cliente}}</p> </div>
             @endif
+          
             <div class="pcpItem-footer">
-                <a href="{{ route('pcp.edit', ['id' => $pcp->id_pcp]) }}" class="btn btn-primary btnEditPCP"><img src="/img/edit_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg" alt="Edit" height="30" width="30"></a>
+                <a href="{{ route('pcp.edit', ['id' => $pcp->id_pcp]) }}" class="btn btn-primary btnEditPCP">
+                    <img src="/img/edit_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg" alt="Edit" height="30" width="30">
+                </a>
                 <form action="{{ route('pcp.destroy', ['id' => $pcp->id_pcp]) }}" method="POST" class="d-inline" onsubmit="return confirm('Tem certeza que deseja deletar este registro?')">
                     @csrf
                     @method('DELETE')
@@ -54,6 +74,7 @@
     </ul>
 </div>
 @endif
+  </body>
 
 <script>
     document.addEventListener("DOMContentLoaded", function() {
